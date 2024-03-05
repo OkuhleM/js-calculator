6c5c4fac670d9cb627f922493d38bd7670f2a721
OkuhleM <lindokuhlea38@gmail.com>
2024-03-05 11:18:55 +0200

    ignore node modules

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Components/Calculator';
+
+const App = () => {
+
+  return (
+    <div className='card-container'>
+     <Calculator/>
+    </div>
+  )
+}
+
+export default App;
